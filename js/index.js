b9d059e1900d04f395cc99bfb791aa246dcedc68
md-rafiul <md.rafiul.pau@gmail.com>
2022-08-23 21:34:45 +0600

    add player on click select using dom

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
-// common function
-
 function getValueFromTextField(elementId){
     const gettingElement = document.getElementById(elementId);
     const getValueString = gettingElement.value;
     const getValue = parseInt(getValueString);
 
-    gettingElement.value = '';
+    // gettingElement.value = '';
     return getValue;  
 }
 
@@ -29,47 +27,79 @@ function setInnerText (elementId, value){
     gettingElement.innerText = value;
 }
 
-// selected player
+function addPlayerOnList(name){
+    const listContainer = document.getElementById('player-list');
+    if(listContainer.childNodes.length >4){
+        alert('you can not add more than five player')
+    this.setAttribute ('disabled', false)
+    }
+    else{
+        const li = document.createElement('li');
+        li.innerText = name;
+        listContainer.appendChild(li);
+    }
+}
 
-let playerName = [];
+function disableSelect (buttonId){
+    const button = document.getElementById(buttonId);
+    button.setAttribute ('disabled', true)
+    button.style.backgroundColor = "gray";
+}
 
-document.getElementById('select').addEventListener('click', function(){
-    playerName.push(getInnerTextString('Messi')) ;
+// player list
+// let hudai = document.getElementsByClassName('hudai')
+
+// .addEventListener('click', function(event){
+//     console.log("shit");
+//     let text = event.target.parentNode.fristChild.innerText;
+//     console.log(text);
+// }) 
+document.getElementById('selectMessi').addEventListener('click', function(){
+    addPlayerOnList(document.getElementById('Messi').innerText);
+    disableSelect('selectMessi');
+})
+document.getElementById('selectNeymar').addEventListener('click', function(){
+    addPlayerOnList(document.getElementById('Neymar').innerText);
+    disableSelect('selectNeymar');
+})
+document.getElementById('selectRonaldo').addEventListener('click', function(){
+    addPlayerOnList(document.getElementById('Ronaldo').innerText);
+    disableSelect('selectRonaldo');
+})
+document.getElementById('selectKylian').addEventListener('click', function(){
+    addPlayerOnList(document.getElementById('Kylian').innerText);
+    disableSelect('selectKylian');
+})
+document.getElementById('selectLewan').addEventListener('click', function(){
+    addPlayerOnList(document.getElementById('Lewan').innerText);
+    disableSelect('selectLewan');
+})
+document.getElementById('selectKane').addEventListener('click', function(){
+    addPlayerOnList(document.getElementById('Kane').innerText);
+    disableSelect('selectKane');
 })
 
 
 
-// Budget section
 
 
 document.getElementById('cal-player-expenses').addEventListener('click', function(){
-
-
-
-
-
-
-
-
-    
-    
-    // const playerExpense = getValueFromTextField('perPlayerBudget');
-    // console.log(playerExpense);
-    // setInnerText('player-expense', playerExpense);
-   
-    // if (isNaN(getValueFromTextField('perPlayerBudget'))){
-    //     console.log(isNaN(getValueFromTextField('perPlayerBudget')));
+    // if(isNaN()){
     //     alert("Please input number!!!");
     // }
-    // else{
-    // }
+    const totalPlayer = document.getElementById('player-list');
+    const length = totalPlayer.childNodes.length;
+    
+
+    let playerExpense = getValueFromTextField('perPlayerBudgetInput')*length;
+    setInnerText('player-expense', playerExpense);
 })
 
 document.getElementById('calculate-total').addEventListener('click', function(){
     let playerExpense = getInnerTextNumeber('player-expense');
     let managerExpense = getValueFromTextField('managerBudget');
     let coachExpense = getValueFromTextField('coachBudget');
-    
+    console.log(playerExpense , managerExpense , coachExpense);
     let total = playerExpense + managerExpense + coachExpense;
     setInnerText('total', total);
 })
